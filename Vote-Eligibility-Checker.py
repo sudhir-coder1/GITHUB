242d225e928a70d2368c6450ad9358ac99092feb
sudhir bind <sudhirbind2702715@gmail.com>
2025-07-14 08:15:58 -0700

    updated

--- Vote-Eligibility-Checker.py
+++ Vote-Eligibility-Checker.py
@@ -5,4 +5,4 @@ age = int(input("Enter your age: "))
 if age >= 18:
     print("✅ You are eligible to vote!")
 else:
-    print("❌ You are not eligible to vote. Come back when you're 18.")
+    print("❌ You are not eligible to vote.")
